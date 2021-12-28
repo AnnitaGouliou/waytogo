1b87e8fad549ed031643749da57f005758611fac
AnnitaGouliou <gouliouannie@gmail.com>
2021-12-28 21:29:28 +0000

    Create PlayerMovement.cs

---
+++ waytogo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
+
+    private void Awake()
+    {       //grab references fro rigidbody and animator from object 
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
+
+    // Flip player when moving left / right 
+        if(horizontalInput > 0.01f) //means the playr is moving right
+            transform.localScale = new Vector3(1.5f, 1.5f, 1f);
+       else if(horizontalInput < -0.01f) //players turns left
+            transform.localScale = new Vector3(-1.5f, 1.5f, 1f);
+
+        if(Input.GetKey(KeyCode.Space) && grounded)
+            Jump();
+
+            //set animator parameteres
+            anim.SetBool("run", horizontalInput !=0);
+            anim.SetBool("grounded", grounded);
+    }
+
+private void Jump()
+{
+
+body.velocity = new Vector2(body.velocity.x, speed);
+anim.SetTrigger("jump");
+grounded = false;
+
+}
+
+private void OnCollisionEnter2D(Collision2D collision)
+{
+        if(collision.gameObject.tag == "Ground")
+            grounded = true;
+}
+
+}
