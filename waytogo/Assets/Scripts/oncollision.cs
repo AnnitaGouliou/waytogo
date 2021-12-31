96bd10e4b5874d92a5e86370ab680cc86e92efce
AnnitaGouliou <gouliouannie@gmail.com>
2021-12-31 00:03:52 +0000

    Update oncollision.cs

--- waytogo/Assets/Scripts/oncollision.cs
+++ waytogo/Assets/Scripts/oncollision.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class oncollision : MonoBehaviour
 {
+    [SerializeField] private AudioSource deathSoundEffect;
+    [SerializeField] private AudioSource finishLevel;
     
 
 private void OnCollisionEnter2D(Collision2D collision)
@@ -12,16 +14,39 @@ private void OnCollisionEnter2D(Collision2D collision)
     if(collision.gameObject.CompareTag("enemy"))
 
     {   
-    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+       Die();
+       Invoke("RestartLevel", 1f);
+
+
     }
 
     else if(collision.gameObject.CompareTag("NextLevel"))
 {
 
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    finishLevel.Play();
+    Invoke("CompleteLevel", 1f);
+  
+  
 }    
 }
 
+private void CompleteLevel()
+{
+  SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+}
+
+private void RestartLevel()
+{
+
+    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
+}
+
+private void Die()
+
+{
+ deathSoundEffect.Play();
+}
 
 
 
